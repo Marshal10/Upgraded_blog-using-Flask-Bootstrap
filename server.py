aac664d4643fbd55385e37bb7731d8eb7aefd0f8
Marshal10 <marshalmaleane123@gmail.com>
2024-01-15 03:16:14 +0530

    Fetch from api and display

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
 from flask import Flask,render_template
-
+import requests
 app=Flask(__name__)
+
+response=requests.get("https://api.npoint.io/f841091ebd32f50c96a0")
+data=response.json()  
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html",posts=data)
 
 @app.route('/about')
 def about():
