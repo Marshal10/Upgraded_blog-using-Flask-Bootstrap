fc7f8319b2f2d718b3b1f8430f2ffa23b85b2cbc
Marshal10 <marshalmaleane123@gmail.com>
2024-01-15 14:01:50 +0530

    Send form data via mail

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask,render_template,request
 import requests
-
+import smtplib
 app=Flask(__name__)
 
 response=requests.get("https://api.npoint.io/935908f9dc4d4516b62b")
@@ -35,6 +35,13 @@ def contact():
         email=dt["email"]
         phone=dt["phone"]
         message=dt["message"]
+        my_email="maleanmarshal@gmail.com"
+        password="tfpkbvtucowmyrin"
+        
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(my_email,password)
+            connection.sendmail(from_addr=my_email,to_addrs="marshalmaleane123@gmail.com",msg=f"Subject:Form Data.\n\nName: {name}\nEmail: {email}\nPhone: {phone}\nMessage: {message}")
         return render_template("contact.html",msg_sent=True)
     else:
         return render_template("contact.html",msg_sent=False)
